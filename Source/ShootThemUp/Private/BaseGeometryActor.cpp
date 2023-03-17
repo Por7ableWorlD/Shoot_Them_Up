80689b8804e13d3d9eda96901525fdc6d46ebf29
Alexander Kuzin <61850604+Por7ableWorlD@users.noreply.github.com>
2023-03-17 21:06:47 +0400

    Base work with UE_LOG()

---
+++ Source/ShootThemUp/Private/BaseGeometryActor.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseGeometryActor.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
+
+// Sets default values
+ABaseGeometryActor::ABaseGeometryActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseGeometryActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Вызываем базовые методы по работе с UE_LOG()
+	//OutputBaseLogs();
+	//OutputStatToLogs();
+}
+
+// Called every frame
+void ABaseGeometryActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseGeometryActor::OutputBaseLogs()
+{
+	int WeaponNum = 5;
+	int KillsNum = 7;
+	float Health = 54.2351f;
+	bool isDead = false;
+	bool HasWeapon = true;
+
+	UE_LOG(LogTemp, Display, TEXT("Weapon number = %d"), WeaponNum);
+	UE_LOG(LogTemp, Display, TEXT("Kills number = %i"), KillsNum);
+	UE_LOG(LogTemp, Display, TEXT("Health = %f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Health = %.2f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("isDead = %d"), isDead);
+	UE_LOG(LogTemp, Display, TEXT("HasWeapon = %d"), static_cast<int>(HasWeapon));
+}
+
+void ABaseGeometryActor::OutputStatToLogs()
+{
+	FString Name = "John Connor";
+	UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *Name);
+
+	int WeaponNum = 5;
+	float Health = 54.2351f;
+	bool isDead = false;
+
+	FString WeaponNumStr = "Weapons num = " + FString::FromInt(WeaponNum);
+	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
+	FString isDeadStr = "Is Dead: " + FString(isDead ? "true" : "false");
+
+	FString Stat = FString::Printf(TEXT(" \n == All Stat: == \n\t%s\n\t%s\n\t%s"),
+		*WeaponNumStr, *HealthStr, *isDeadStr);
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("%s"), *Stat);
+}
+
